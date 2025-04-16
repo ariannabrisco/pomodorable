3f6dd2b2453526d0cf60bb182324b712319144c8
Arianna Brisco <121651388+ariannabrisco@users.noreply.github.com>
2025-04-15 22:30:26 -0500

    pomodoro loop logic

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-// consts
+// ~ CONSTS ~
 const startEl = document.getElementById("start")
-const stopEl = document.getElementById("stop")
+const pauseEl = document.getElementById("pause")
 const resetEl = document.getElementById("reset")
 const timerEl = document.getElementById("timer")
+const timerTypeEl = document.getElementById("timerType")
 
-// lets
-let interval
+
+// ~ LETS ~
+let interval;
 let timeLeft = 1500; // seconds
+let workCount = 0;
+let breakCount = 0;
+let timerType = "Work";
 
-// functions
+var timerTypeOptions = {
+    Work: 1500,
+    ShortBreak: 300,
+    LongBreak: 900
+}
+
+
+// ~ FUNCTIONS ~
+// timer functions
 function updateTimer(){
     let minutes = Math.floor(timeLeft / 60);
     let seconds = timeLeft % 60;
     let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-
     timerEl.innerHTML = formattedTime
+    timerTypeEl.innerHTML = timerType
 }
 
 function startTimer(){
@@ -23,25 +36,47 @@ function startTimer(){
         updateTimer();
         if(timeLeft === 0){
             clearInterval(interval);
-            alert("Time's up!");
-            timeLeft = 1500;
+            calcNextTimerType();
             updateTimer();
         }
-    }, 1000)
+    }, 10)      // 1000 for actual, 10 for testing
 }
 
-function stopTimer(){
+function pauseTimer(){
     clearInterval(interval);
-
 }
 
 function resetTimer(){
     clearInterval(interval);
-    timeLeft = 1500;
+    timeLeft = timerTypeOptions[timerType];
     updateTimer();
 }
 
-// calls
+function calcNextTimerType(){
+    if(timerType == "Work"){
+        workCount += 1;
+        alert(timerType + " time's up!\nWork Count: " + workCount + "\nShort Break Count: " + breakCount);
+        if(breakCount != 0 && breakCount % 3 === 0){
+            switchTimerType("LongBreak");
+        } else if(breakCount != 0 && breakCount % 4 === 0){
+            alert("Pomodoro Session Complete! Great Job!")
+        } else {
+            switchTimerType("ShortBreak");
+        }
+    } else {
+        breakCount += 1;
+        alert(timerType + " time's up!\nWork Count: " + workCount + "\nShort Break Count: " + breakCount);
+        switchTimerType("Work");
+    }
+    console.log(workCount, breakCount);
+}
+
+function switchTimerType(type){
+    timerType = type;
+    resetTimer();
+}
+
+// ~ CALLS ~ 
 startEl.addEventListener("click", startTimer)
-stopEl.addEventListener("click", stopTimer)
+pauseEl.addEventListener("click", pauseTimer)
 resetEl.addEventListener("click", resetTimer)
