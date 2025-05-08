041acf780eb111e869d5d4c0280d583bbdc8c7b6
Arianna Brisco <121651388+ariannabrisco@users.noreply.github.com>
2025-05-08 09:59:09 -0500

    more beaut

--- main.js
+++ main.js
@@ -236,7 +236,7 @@ function updateControlButtons(isrunning){
 
 // sidebar control functions
 function openSidebar(){
-    sidebarEl.style.width = "200px";
+    sidebarEl.style.width = "220px";
 }
 
 function closeSidebar(){
